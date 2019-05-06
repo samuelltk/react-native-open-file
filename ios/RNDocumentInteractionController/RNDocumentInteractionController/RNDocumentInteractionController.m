cab30003bfaf89e39230f4a68ecadb84615ae55b
samuelltk <luuyangg@126.com>
2019-05-06 15:11:49 +0800

    Update RNDocumentInteractionController.m
    如果预览失败，则会调用系统的拷贝到第三方的 APP 去打开文档。

--- ios/RNDocumentInteractionController/RNDocumentInteractionController/RNDocumentInteractionController.m
+++ ios/RNDocumentInteractionController/RNDocumentInteractionController/RNDocumentInteractionController.m
@@ -18,11 +18,26 @@
 
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(open: (NSURL *)path)
+RCT_EXPORT_METHOD(open: (NSURL *)path resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
 {
     UIDocumentInteractionController *interactionController = [UIDocumentInteractionController interactionControllerWithURL:path];
     interactionController.delegate = self;
-    [interactionController presentPreviewAnimated:YES];
+    BOOL canPreview=[interactionController presentPreviewAnimated:YES];
+    if(!canPreview){
+        UIViewController *ctrl = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+        
+        BOOL wasOpened = [interactionController presentOpenInMenuFromRect:ctrl.view.bounds inView:ctrl.view animated:YES];
+        if(wasOpened){
+            resolve(@"other app can open this file");
+        }else{
+            //dont open this file
+            NSError *error = [NSError errorWithDomain:@"Open error" code:500 userInfo:nil];
+            reject(@"Open error", @"Open error", error);
+        }
+    }else{
+        resolve(@"can preview");
+    }
 }
 
 - (UIViewController *) documentInteractionControllerViewControllerForPreview: (UIDocumentInteractionController *) controller
